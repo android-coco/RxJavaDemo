ae5d615f17d5c4b9803c72b58220e72e23033e09
youhao <youh@fitcome.net>
2017-10-11 19:02:32 +0800

    Initial commit

---
+++ app/src/main/java/yh/org/rxjavademo/MainActivity.java
@@ -0,0 +1,61 @@
+package yh.org.rxjavademo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final String TAG = getClass().getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Observable.create(new ObservableOnSubscribe<Integer>()
+        {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception
+            {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>()
+        {
+            @Override
+            public void onSubscribe(@NonNull Disposable d)
+            {
+                Log.e(TAG, "subscribe");
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer)
+            {
+                Log.e(TAG, "onNext");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e)
+            {
+                Log.e(TAG, "onError");
+            }
+
+            @Override
+            public void onComplete()
+            {
+                Log.e(TAG, "onComplete");
+            }
+        });
+
+
+    }
+}
